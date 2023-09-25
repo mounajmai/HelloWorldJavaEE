a6df029bf06f29fa49e8baf04cffc8dda3f5112e
mounajmai <130464507+mounajmai@users.noreply.github.com>
2023-09-25 11:21:23 +0200

    version de tp1 JEE

---
+++ src/main/java/net/codejava/javaee/HelloServlet.java
@@ -0,0 +1,36 @@
+package net.codejava.javaee;
+
+import java.io.IOException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		// TODO Auto-generated method stub
+		String yourName = request.getParameter("yourName");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.close();
+
+	}
+
+}
